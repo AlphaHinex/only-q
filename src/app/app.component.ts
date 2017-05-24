7a2d97fb7d6413e0cd54ba7e097bde3d35f186ad
AlphaHinex <AlphaHinex@gmail.com>
2017-05-24 14:18:42 +0800

    Export options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,13 @@ import {AppInputDialogComponent, Ques} from './app-input-dialog.component';
 import {AppViewDialogComponent} from './app-view-dialog.component';
 import {Headers, Http, RequestOptions} from '@angular/http';
 
-export const headers = new Headers({
+const headers = new Headers({
   'X-LC-Id': 'ncucSqWquNS5qSBCNrqEhA8O',
-  'X-LC-Key': 'd6cARxGcTwgyi0IGz7ss7LHp'
+  'X-LC-Key': 'd6cARxGcTwgyi0IGz7ss7LHp',
+  'Content-Type': 'application/json'
+});
+export const options = new RequestOptions({
+  headers: headers,
 });
 
 @Component({
@@ -20,9 +24,6 @@ export class AppComponent implements OnInit {
   constructor(public dialog: MdDialog, public snackBar: MdSnackBar, private http: Http) { }
 
   getQues(): void {
-    const options = new RequestOptions({
-      headers: headers,
-    });
     const param = encodeURI('cql=select * from Ques limit 0,100 order by -score, -createdAt');
     this.http.get('https://api.leancloud.cn/1.1/cloudQuery?' + param, options)
               .map(res => res.json())
@@ -38,10 +39,6 @@ export class AppComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result && result.name && result.ques) {
         const snackBar = this.snackBar;
-        headers.append('Content-Type', 'application/json');
-        const options = new RequestOptions({
-          headers: headers,
-        });
         this.http.post('https://api.leancloud.cn/1.1/classes/Ques', result, options)
                   .subscribe(() => {
                     snackBar.open('Dear ' + result.name + ', thanks for your feedback!', '', {duration: 2000});
